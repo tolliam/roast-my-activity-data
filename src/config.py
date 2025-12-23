acddff8cec2c4fac99b8af25e547cba59e2c162e
Tolliam <44102350+tolliam@users.noreply.github.com>
2025-12-23 15:51:44 +0000

    Bump version to 0.7.0

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ settings used throughout the application.
 from typing import Dict
 
 # Version
-VERSION = "0.5.0"
+VERSION = "0.7.0"
 
 # Application Settings
 APP_TITLE = "🔥 Roast My Activity Data"
